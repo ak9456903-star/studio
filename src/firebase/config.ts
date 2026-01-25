8d3c6dffd3487b54762c8ea4134063c7e7e68a9d
Ankita Roy <ankitaroy032009@gmail.com>
2026-01-25 10:54:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-707281451-b14e6",
+  "appId": "1:587589770466:web:094033da206995b69892bb",
+  "apiKey": "AIzaSyCWGcYK7HHmqTECa50NYLAtyYLD7jEPuQ8",
+  "authDomain": "studio-707281451-b14e6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "587589770466"
+};
